f2ddb4cfa34a897b1531461baea95f1128fc7951
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-06-02 15:08:25 +0200

    Java - Practice #4
    Another test, added a new project with a class

---
+++ TP1/src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Postavljen novi projekt");
+		System.out.println("Pazi na foldere i sto radis");
+		
+	}
+	
+}
