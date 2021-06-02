ed3b2d44f2d3558239ce32e98038cd49f6439677
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-06-02 14:36:31 +0200

    Java - Practice #3
    Another test, added a new class

--- Edunova01/src/Hello.java
+++ Edunova01/src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("Hello Edunova");
 		System.out.println("Osijek");
 		System.out.println("Linija da vidim jel misli to prebacit via github");
+		System.out.println("Ok to je radilo, još da skontam di sam zeznuo s gitignore");
 		
 	}
 
---
+++ Edunova01/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+		public static void main(String[] args) {
+			System.out.println("Stavljen novi class u sklopu isprobavanja");
+			System.out.println("Sjeti se space + ctrl");
+			System.out.println("Odradi jedan run");
+			System.out.println("I debug");
+			System.out.println("Ok to je to zasad");
+		}
+	}
