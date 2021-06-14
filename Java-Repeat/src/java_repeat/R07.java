38850ff643c722cbfcac2ea7d425f5855e857ec9
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-06-14 14:34:35 +0200

    Repeat from Edunova 03 i 04.

---
+++ Java-Repeat/src/java_repeat/R07.java
@@ -0,0 +1,37 @@
+package java_repeat;
+
+public class R07 {
+	
+	public static void main(String[] args) {
+		
+		// Nadopuna E03
+		// ln dio znaci novi red
+		
+		System.out.println("Text pa novi red");
+		
+		// space u drugom printu je radi razmaka u rezultatu
+		
+		System.out.print("Text");
+		System.out.print(" u istom redu");
+		
+		// s ovim dobijem različite razmake
+		
+		System.out.print("\n\tOsijek");
+		
+		System.out.println("\tOsijek");
+		
+		System.out.println("\nOsijek");
+		
+		//ascii art
+		
+		System.out.println("           _  wWw  wWw\\\\\\  ///   .-.   wWw    wWw       \r\n"
+				+ "   wWw   _||\\ (O)  (O)((O)(O)) c(O_O)c (O)    (O)  /)   \r\n"
+				+ "   (O)_ (_'\\  / )  ( \\ | \\ || ,'.---.`,( \\    / )(o)(O) \r\n"
+				+ "  .' __).'  |/ /    \\ \\||\\\\||/ /|_|_|\\ \\\\ \\  / /  //\\\\  \r\n"
+				+ " (  _) ((_) || \\____/ ||| \\ || \\_____/ |/  \\/  \\ |(__)| \r\n"
+				+ "  `.__) `-`.)'. `--' .`||  ||'. `---' .`\\ `--' / /,-. | \r\n"
+				+ "           (   `-..-' (_/  \\_) `-...-'   `-..-' -'   '' ");
+		
+	}
+
+}
