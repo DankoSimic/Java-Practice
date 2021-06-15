f9701777c13c3d7467addf75e25d4a4b26e84189
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-06-15 21:45:59 +0200

    Commentary for self help.

--- Java-Repeat/src/java_repeat/R10.java
+++ Java-Repeat/src/java_repeat/R10.java
@@ -6,7 +6,11 @@ public class R10 {
 
 	public static void main(String[] args) {
 		
+		// Dvodimenzionalni niz - tablica/matrica
+		
 		int [][] tablica = new int [3][3];
+		
+		// U tablici od 9 polja, prvi broj u uglatoj je row drugi column
 		
 		tablica [0][0] = 1;
 		tablica [0][1] = 0;
@@ -26,6 +30,9 @@ public class R10 {
 		System.out.println(Arrays.toString(tablica [1]));
 		System.out.println(Arrays.toString(tablica [2]));
 		
+		// Trodimenzionalni niz - kocka
+		// Moguće pohraniti 1000 elemenata
+		
 		int [][][] kocka = new int [10][10][10];
 		
 		int [][][][] kocka_u_vremenu = new int [10][10][10][10];
@@ -36,7 +43,14 @@ public class R10 {
 				{3,4,5,6}
 		};
 		
+		// Ova varijanta s vitičastom stvara niz s zadanim brojem polja i unosima
+		// Dodatni print linovi su tu jer sam pokušavao shvatit kako zapravo sve radi
+		
 		System.out.println(podaci[1][2]);
+		System.out.println(podaci[0][0]);
+		System.out.println(podaci[0][1]);
+		System.out.println(podaci[0][2]);
+		System.out.println(podaci[0][3]);
 
 	}
 	
