a1f769a5f80ea989e6b75fbc65cd11ae4fba7e9d
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-06-21 12:41:10 +0200

    Tasks from Edunova 07 Part 3.

--- Java-Task/src/java_task/Tasks04.java
+++ Java-Task/src/java_task/Tasks04.java
@@ -6,7 +6,8 @@ public class Tasks04 {
 	
 	public static void main(String[] args) {
 		
-		//Program unosi 2 broja i ispisuje zbroj
+		// Zadatak 1
+		// Program unosi 2 broja i ispisuje zbroj
 		
 		int a,b,c;
 		a = 2;
@@ -15,6 +16,7 @@ public class Tasks04 {
 		
 		System.out.println(c);
 		
+		// Zadatak 2
 		// program od korisnika unosi 2 decimalna broja
 		// ispisuje veći
 		
@@ -31,19 +33,49 @@ public class Tasks04 {
 			
 		}
 		
+		// Zadatak 3
 		// Program ispisuje sve brojeve od 100 do 21
 		// jedno do drugoga odvojeno zarezom
 		
-		// Nađi poslije za zarez
-		// Preumoran za noćni 
+		// Riješio zarez
 		
 		int f,g;
 		f = 21;
 		g = 100;
 		
 		for (int i=g;i>=f;i--) {
-			System.out.println(i );}
+			System.out.println(i + "," );}
 		
-	}
+		// Zadatak 12
+		// Program ispisuje "Edunova" (ispisuju se i navodnici)
+		
+		System.out.println("\"Edunova\"");
+		
+		// Zadatak 10
+		// Ispiši Sve parne brojeve od 3 do 47
+		
+		int h,j;
+		h = 3;
+		j = 47;
+		
+		for(int i=h;i>=j;i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		// Zadatak 4
+		// Program učitava cijele brojeve od korisnika
+		// sve dok se ne učita broj 0. Tada se program završava
+		
+		int k;
+		
+		for(;;) { k = Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli broj, program završava kad upišeš 0"));
+			if ( k == 0 )
+				break;
+		}
+			}
+		}
 
-}
+
+
